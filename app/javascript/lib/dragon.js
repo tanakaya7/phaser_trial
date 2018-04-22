4deb6d6769c6435f6950abbf2d8eccd297f72078
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-04-22 23:30:34 +0900

    [dragon] Put images on the map

--- app/javascript/lib/dragon.js
+++ app/javascript/lib/dragon.js
@@ -1,1 +1,41 @@
 import Phaser from "phaser/dist/phaser.min"
+import BackgroundImage from "../images/background.png"
+import PlayerSprite from "../images/player-sprite.png"
+
+const config = {
+  type: Phaser.AUTO,
+  parent: "#screen",
+  width: 640,
+  height: 640,
+  scene: {
+    key: "main",
+    preload: preload,
+    create: create
+  }
+}
+
+const game = new Phaser.Game(config)
+
+function preload() {
+  this.load.image("background", BackgroundImage)
+  this.load.spritesheet("player", PlayerSprite, {
+    frameWidth: 64, frameHeight: 64
+  })
+}
+
+function create() {
+  this.add.group({
+    key: "background",
+    repeat: 9,
+    setXY: {
+      x: 32,
+      y: 32,
+      stepX: 64,
+      stepY: 0
+    }
+  })
+
+  const player = this.add.sprite(0, 0, "player")
+  player.setOrigin(0, 0)
+  player.setFrame(0)
+}
