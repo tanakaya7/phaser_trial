1212ca7d50cd4ea53c107536c21ca9bae29086f2
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-04-23 11:02:58 +0900

    [dragon] keydownEventHandler

--- app/javascript/dragon/create.js
+++ app/javascript/dragon/create.js
@@ -1,16 +1,38 @@
+let player
+
 export default function create() {
   this._map = this.make.tilemap({key: "map"})
   const background = this._map.addTilesetImage("background")
   const groundLayer =
     this._map.createDynamicLayer("World", background, 0, 0);
 
-  this._player = this.add.sprite(0, 0, "player")
+  player = this.add.sprite(0, 0, "player")
+  this._player = player
   this._player.setOrigin(0, 0)
   this._player.setFrame(0)
+  this._player._x = 0
+  this._player._y = 0
 
   this.cameras.main.setBounds(0, 0,
     this._map.widthInPixels, this._map.heightInPixels);
   this.cameras.main.startFollow(this._player);
 
-  this._cursors = this.input.keyboard.createCursorKeys()
+  window.document.onkeydown = keydownEventHandler
+}
+
+function keydownEventHandler(event){
+  if (event.key === "ArrowLeft") {
+    player.setFrame(0)
+    if (player._x > 0) player._x--
+  }
+  else if (event.key === "ArrowRight") {
+    player.setFrame(1)
+    if (player._x < 9) player._x++
+  }
+  else if (event.key === "ArrowUp") {
+    if (player._y > 0) player._y--
+  }
+  else if (event.key === "ArrowDown") {
+    if (player._y < 9) player._y++
+  }
 }
--- app/javascript/dragon/update.js
+++ app/javascript/dragon/update.js
@@ -1,18 +1,20 @@
 export default function update(time, delta) {
-  if (this._cursors.left.isDown) {
-    this._player.setFrame(0)
-    if (this._player.x > 0) this._player.setX(this._player.x - 4)
-  }
-  else if (this._cursors.right.isDown) {
-    this._player.setFrame(1)
-    if (this._player.x < 640 - 64) this._player.setX(this._player.x + 4)
-  }
-  else if (this._cursors.up.isDown) {
-    if (this._player.y > 0) this._player.setY(this._player.y - 4)
-  }
-  else if (this._cursors.down.isDown) {
-    if (this._player.y < 640 - 64) this._player.setY(this._player.y + 4)
-  }
-  else {
-  }
+  this._player.x = this._player._x * 64
+  this._player.y = this._player._y * 64
+  // if (this._cursors.left.isDown) {
+  //   this._player.setFrame(0)
+  //   if (this._player.x > 0) this._player.setX(this._player.x - 4)
+  // }
+  // else if (this._cursors.right.isDown) {
+  //   this._player.setFrame(1)
+  //   if (this._player.x < 640 - 64) this._player.setX(this._player.x + 4)
+  // }
+  // else if (this._cursors.up.isDown) {
+  //   if (this._player.y > 0) this._player.setY(this._player.y - 4)
+  // }
+  // else if (this._cursors.down.isDown) {
+  //   if (this._player.y < 640 - 64) this._player.setY(this._player.y + 4)
+  // }
+  // else {
+  // }
 }
