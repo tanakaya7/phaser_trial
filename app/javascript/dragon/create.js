ed3e5a733824e6693ed56094740139d27e892aff
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-04-23 11:20:00 +0900

    [dragon] Refactor create.js

--- app/javascript/dragon/create.js
+++ app/javascript/dragon/create.js
@@ -6,33 +6,45 @@ export default function create() {
   const groundLayer =
     this._map.createDynamicLayer("World", background, 0, 0);
 
-  player = this.add.sprite(0, 0, "player")
-  this._player = player
-  this._player.setOrigin(0, 0)
-  this._player.setFrame(0)
-  this._player._x = 0
-  this._player._y = 0
+  createPlayer(this)
+  prepareCamera(this)
 
-  this.cameras.main.setBounds(0, 0,
-    this._map.widthInPixels, this._map.heightInPixels);
-  this.cameras.main.startFollow(this._player);
-
-  window.document.onkeydown = keydownEventHandler
+  window.document.onkeydown = event => {
+    const h = keydownEventHandlers[event.key]
+    if (h) h(event)
+  }
 }
 
-function keydownEventHandler(event){
-  if (event.key === "ArrowLeft") {
+function createPlayer(game) {
+  player = game.add.sprite(0, 0, "player")
+
+  player.setOrigin(0, 0)
+  player.setFrame(0)
+  player._x = 0
+  player._y = 0
+
+  game._player = player
+}
+
+function prepareCamera(game) {
+  game.cameras.main.setBounds(0, 0,
+    game._map.widthInPixels, game._map.heightInPixels);
+  game.cameras.main.startFollow(game._player);
+}
+
+const keydownEventHandlers = {
+  "ArrowLeft": event => {
     player.setFrame(0)
     if (player._x > 0) player._x--
-  }
-  else if (event.key === "ArrowRight") {
+  },
+  "ArrowRight": event => {
     player.setFrame(1)
     if (player._x < 9) player._x++
-  }
-  else if (event.key === "ArrowUp") {
+  },
+  "ArrowUp": event => {
     if (player._y > 0) player._y--
-  }
-  else if (event.key === "ArrowDown") {
+  },
+  "ArrowDown": event => {
     if (player._y < 9) player._y++
   }
 }
