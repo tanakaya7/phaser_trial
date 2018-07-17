9e19943c41611349a0f50213f1cb7f1f360da501
Tsutomu Kuroda <=>
2018-07-17 22:11:31 +0900

    [dragon] Add timestamp to json.file URL

--- app/javascript/dragon/cave/preload.js
+++ app/javascript/dragon/cave/preload.js
@@ -2,7 +2,7 @@ import TilesSprite from "../../images/tiles-sprite.png"
 import PlayerSprite from "../../images/player-sprite.png"
 
 export default function preload() {
-  this.load.tilemapTiledJSON("cave_map", "/dragon/cave_map.json")
+  this.load.tilemapTiledJSON("cave_map", "/dragon/cave_map.json?t=" + new Date())
   this.load.spritesheet("tiles", TilesSprite, {
     frameWidth: 64, frameHeight: 64
   })
--- app/javascript/dragon/main/preload.js
+++ app/javascript/dragon/main/preload.js
@@ -2,7 +2,7 @@ import TilesSprite from "../../images/tiles-sprite.png"
 import PlayerSprite from "../../images/player-sprite.png"
 
 export default function preload() {
-  this.load.tilemapTiledJSON("main_map", "/dragon/main_map.json")
+  this.load.tilemapTiledJSON("main_map", "/dragon/main_map.json?t=" + new Date())
   this.load.spritesheet("tiles", TilesSprite, {
     frameWidth: 64, frameHeight: 64
   })
