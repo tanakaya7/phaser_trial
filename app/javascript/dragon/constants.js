4b6a5dd00f43424e80a88826cd68a12523f7c557
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-08-02 02:14:05 +0900

    [dragon] Make player walk

--- app/javascript/dragon/constants.js
+++ app/javascript/dragon/constants.js
@@ -8,3 +8,12 @@ export const FLOOR = 7
 export const EXIT = 8
 export const TREASURE = 9
 export const COLLISION_TILES = [RIM, WALL]
+
+export const BACK_1 = 0
+export const BACK_2 = 2
+export const RIGHT_1 = 3
+export const RIGHT_2 = 5
+export const FRONT_1 = 6
+export const FRONT_2 = 8
+export const LEFT_1 = 9
+export const LEFT_2 = 11
--- app/javascript/dragon/event_handlers.js
+++ app/javascript/dragon/event_handlers.js
@@ -63,21 +63,37 @@ function SpaceKeydownOnCave(scene, event) {
 }
 
 function ArrowLeft(scene, event) {
-  scene._player.setFrame(9)
+  if (scene._player.frame.name === CONSTANTS.LEFT_1)
+    scene._player.setFrame(CONSTANTS.LEFT_2)
+  else
+    scene._player.setFrame(CONSTANTS.LEFT_1)
+
   if (movable_to(scene, -1, 0)) scene._player._x--
 }
 
 function ArrowRight(scene, event) {
-  scene._player.setFrame(5)
+  if (scene._player.frame.name === CONSTANTS.RIGHT_1)
+    scene._player.setFrame(CONSTANTS.RIGHT_2)
+  else
+    scene._player.setFrame(CONSTANTS.RIGHT_1)
+
   if (movable_to(scene, 1, 0)) scene._player._x++
 }
 
 function ArrowUp(scene, event) {
-  scene._player.setFrame(2)
+  if (scene._player.frame.name === CONSTANTS.BACK_1)
+    scene._player.setFrame(CONSTANTS.BACK_2)
+  else
+    scene._player.setFrame(CONSTANTS.BACK_1)
+
   if (movable_to(scene, 0, -1)) scene._player._y--
 }
 
 function ArrowDown(scene, event) {
-  scene._player.setFrame(6)
+  if (scene._player.frame.name === CONSTANTS.FRONT_1)
+    scene._player.setFrame(CONSTANTS.FRONT_2)
+  else
+    scene._player.setFrame(CONSTANTS.FRONT_1)
+
   if (movable_to(scene, 0, 1)) scene._player._y++
 }
