2fbe69d1656380bfe53c5fd3c921f3f4d412d5b1
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-08-02 01:42:51 +0900

    [dragon] player.setOrigin

--- app/javascript/dragon/main/create.js
+++ app/javascript/dragon/main/create.js
@@ -12,7 +12,7 @@ export default function create() {
 function createPlayer(scene) {
   const player = scene.add.sprite(0, 0, "player")
 
-  player.setOrigin(0, 0)
+  player.setOrigin(-0.2, 0.1)
   player.setFrame(0)
   player._x = 1
   player._y = 1
